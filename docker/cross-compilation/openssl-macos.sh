73e5b75c47be4e408f53495623bd5e428ea4b8fa
aeweda <mraeweda@gmail.com>
2024-11-20 08:35:45 +0400

    chore: fix script

--- docker/cross-compilation/openssl-macos.sh
+++ docker/cross-compilation/openssl-macos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-brew install aarch64-linux-gnu-gcc
+brew install aarch64-elf-gcc
 
 # Set the target and compiler
 TARGET=$1
 CROSS_COMPILE=$2
 
 # Download and extract OpenSSL
-curl -O https://www.openssl.org/source/openssl-1.0.2p.tar.gz
+wget -O https://www.openssl.org/source/openssl-1.0.2p.tar.gz
 tar -xzf openssl-1.0.2p.tar.gz
 cd openssl-1.0.2p
 
